6a600976cde18744c9829788f395c2bdc07a941c
Regis Mucyo <mucyoregis250@gmail.com>
2026-01-01 19:51:51 +0200

    Question 2 about handling errors in promise

--- index.js
+++ index.js
@@ -3,9 +3,19 @@
 // Question 1 : Handling Errors
 
 var p = new Promise((resolve, reject) => {
-  reject("The Fails!");
+  reject(Error("The Fails!"));
 });
 p.catch((error) => console.log(error.message));
 p.catch((error) => console.log(error.message));
 
 // What will the output be for above the above code?
+
+// Question 2 : Handling Errors
+
+var p = new Promise((resolve, reject) => {
+  return Promise.reject(Error("The Fails!"));
+});
+p.catch((error) => console.log(error.message));
+p.catch((error) => console.log(error.message));
+
+// What will the output be for the above code
