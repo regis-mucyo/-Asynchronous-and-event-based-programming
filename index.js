6134dc4d3a575982091c47848b4c260413bfe92f
Regis Mucyo <mucyoregis250@gmail.com>
2026-01-04 20:25:50 +0200

    Answers for all 4  questions

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 var p = new Promise((resolve, reject) => {
   reject(Error("The Fails!"));
 });
-p.catch((error) => console.log(error.message));
-p.catch((error) => console.log(error.message));
+p.catch((error) => console.log(error.message)); // The Fails!
+p.catch((error) => console.log(error.message)); // The Fails!
 
 // What will the output be for above the above code?
+
+// The output will be "The Fails!" Twice
 
 // Question 2 : Handling Errors
 
@@ -20,6 +22,8 @@ p.catch((error) => console.log(error.message));
 
 // What will the output be for the above code
 
+// The output be error called "unhandledPromise" since we are not returning the resolve or reject
+
 // Question 3 : Handling Errors
 
 var p = new Promise((resolve, reject) => {
@@ -30,6 +34,9 @@ var p = new Promise((resolve, reject) => {
 
 // What will the output be for the above code
 
+// The output will be 'The Fails!' and undefined, it is
+// because we are not returning anything from catch
+
 // Question 4 : Handling Errors
 
 var p = new Promise((resolve, reject) => {
@@ -39,3 +46,5 @@ var p = new Promise((resolve, reject) => {
   .catch((error) => console.log(error.message));
 
 // What will the output be for the above code
+
+// The output will be error because chaining catch is not possible
