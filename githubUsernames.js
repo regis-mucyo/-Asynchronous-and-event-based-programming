291f04773c493cd6129abe64c97386a0febe80c6
Regis Mucyo <mucyoregis250@gmail.com>
2026-01-07 08:20:33 +0200

    Async Exercises for fetch github usernames and then get the fullname

---
+++ githubUsernames.js
@@ -0,0 +1,16 @@
+//   https://api.github.com/users/USERNAME
+const fetchGitHubName = async (username) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const result = await response.json();
+    if (!response.ok) {
+      console.log(null);
+      return;
+    }
+    let name = result.name;
+    console.log(name);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+fetchGitHubName("regis-mucyo"); // Regis Mucyo
